2f8deff2554ca98203e3274ff551c65ce40506a5
firat yolal <firatyll213@gmail.com>
2024-12-16 18:00:11 +0300

    route fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,9 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.json({ msg: 'Twitter Clone Backend Running' });
+});
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
